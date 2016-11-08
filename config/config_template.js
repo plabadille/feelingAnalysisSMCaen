1434559b7152c5b326ff3b66b7f142b0fc767761
Lénaïc Couëllan <lcouellan@gmail.com>
2016-11-08 17:20:15 +0100

    connect mongo to node

--- config/config_template.js
+++
@@ -1,7 +0,0 @@
-// Mongoi
-
-const host = 'yourHost';
-const port = 'yourPort';
-const database = 'yourDatabase';
-const user = 'yourUsername';
-const password = 'yourPassword';
--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const config = require('../config/config.json');
 const express = require("express");
 const path = require("path");
 const mongoClient = require('mongodb').MongoClient;
@@ -6,20 +7,20 @@ const app = express();
 app.use(express.static(path.join(__dirname,"../client")));
 
 app.get('/data.json', (req, res) => {
-	mongoClient.connect("mongodb://localhost:28017/seriebox", (err, db) => {
+mongoClient.connect('mongodb://'+config.user+':'+config.password+'@'+config.host+':'+config.port+'/'+config.database, {auth:{authdb:"admin"}}, function (err, db) {
 		if (err) {
 			return console.dir(err);
 		}
-		const bikesCollection = db.collection('series');
-		bikesCollection.find().toArray((err, items) => {
+		console.log(db);
+
+		const collection = db.collection('parseTweets');
+		collection.find().toArray((err, items) => {
 			res.json(items);
 			db.close();
 		});
 	});
-
 })
 
 app.listen(3000, () => {
 	console.log("Le serveur est en train d'écouter...");
 });
-
