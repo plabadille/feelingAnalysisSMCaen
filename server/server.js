f3dc305e81c2c321bd40e53426aa72ab0b29eaa9
lcouellan <lcouellan@gmail.com>
2016-11-11 16:28:49 +0100

    Architecture Twigé

--- server/server.js
+++ server/server.js
@@ -1,25 +1,31 @@
 const config = require('../config/config.json');
-const express = require("express");
-const app = express();
 const path = require("path");
 const mongo = require('./mongo.js');
 const dbGraph = require('./dbGraph.js');
 const dbSearch = require('./dbSearch.js');
-
+var Twig = require("twig"),
+    express = require('express'),
+    app = express();
 
 
 app.use(express.static(path.join(__dirname,"../client")));
-
+app.set('views', path.join(__dirname, '../client/views/'));
 app.use(dbGraph);
 app.use(dbSearch);
-app.get('/data.json', (req, res) => {
-
-	mongo.getTweet((tweets) => {
-		console.dir(tweets);
-		res.json(tweets);
+app.get('/', (req, res) => {
+	mongo.getParseTweets((tweets) => {
+		res.render('indexGraph.twig', {
+    		//tweets : tweets
+  		});
 	});
 
 });
+ 
+/*app.get('/', function(req, res) {
+    res.render('index.twig', {
+    	message : "Hello World"
+  	});
+});*/
 
 mongo.init().then(() => {
 	app.listen(3000, () => {
