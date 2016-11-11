45d5263938bb0a1843bd9ba4552677f7b4adde8f
lcouellan <lcouellan@gmail.com>
2016-11-11 14:09:45 +0100

    Modif du server JS

--- server/server.js
+++ server/server.js
@@ -7,14 +7,21 @@ const app = express();
 app.use(express.static(path.join(__dirname,"../client")));
 
 app.get('/data.json', (req, res) => {
-mongoClient.connect('mongodb://'+config.user+':'+config.password+'@'+config.host+':'+config.port+'/'+config.database, {auth:{authdb:"admin"}}, function (err, db) {
+mongoClient.connect('mongodb://'+config.host+':'+config.port+'/'+config.database, {auth:{authdb:"admin"}}, function (err, db) {
 		if (err) {
 			return console.dir(err);
 		}
-		console.log(db);
 
 		const collection = db.collection('parseTweets');
-		collection.find().toArray((err, items) => {
+		/*collection.find({"label":"pos"}).toArray((err, items) => {
+			res.json(items);
+			db.close();
+		});
+		collection.find({"label":"pos"}).count((err, items) => {
+			res.json(items);
+			db.close();
+		});*/
+		collection.find({"label":"neg"}).count((err, items) => {
 			res.json(items);
 			db.close();
 		});
